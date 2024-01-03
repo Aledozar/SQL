2bbfe4bd49293792d0ae5bf9b814bb9671f05f53
Alejandra <155027985+Aledozar@users.noreply.github.com>
2024-01-02 23:31:56 -0500

    Created my first SQL script

--- Movie_DB_MySQL.sql
+++ Movie_DB_MySQL.sql
@@ -1,5 +1,28 @@
-SELECT m.title, m.director, m.year_release, g.genre_name
+CREATE TABLE movies (
+    movie_id INT AUTO_INCREMENT PRIMARY KEY,
+    title TEXT NOT NULL,
+    director TEXT,
+    year_released INTEGER,
+    genre_id INTEGER,
+    FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
+);
+
+CREATE TABLE genres (
+    genre_id INTEGER PRIMARY KEY,
+    genre_name TEXT NOT NULL
+);
+
+INSERT INTO genres (genre_name) VALUES ('Action'), ('Comedy'), ('Drama'), ('Fantasy'), ('Horror');
+
+INSERT INTO movies (title, director, year_released, genre_id) VALUES 
+('Inception', 'Christopher Nolan', 2010, (SELECT genre_id FROM genres WHERE genre_name = 'Action')),
+('The Grand Budapest Hotel', 'Wes Anderson', 2014, (SELECT genre_id FROM genres WHERE genre_name = 'Comedy')),
+('Parasite', 'Bong Joon-ho', 2019, (SELECT genre_id FROM genres WHERE genre_name = 'Drama'));
+
+SELECT * FROM movies;
+
+SELECT m.title, m.director, m.year_released, g.genre_name 
 FROM movies m
-JOIN genres g
+JOIN genres g 
 ON m.genre_id = g.genre_id
-where g.genre_name = 'comedy';
+WHERE g.genre_name = 'Comedy';
